d47844e1ffa191c5dc467fb63893364da2b9bd6b
Chad Rutherford <c.ruth544@gmail.com>
2015-11-24 21:31:02 -0800

    finished atm, but not quite as beautiful with overdraft feature... ToT"

--- w02/d02/atm_hw/javascripts/main.js
+++ w02/d02/atm_hw/javascripts/main.js
@@ -1,11 +1,13 @@
 $(function  () {
   $('body').css('visibility', 'visible')
+  $('.balance').addClass('zero')
 
-  function handleDollarString (balance, operator, input) {
+  function handleDollarString (balance, operator, input, overdraft) {
     balance           = Number(balance.substr(1))
     input             = Number(input)
     var returnString  = '$'
-    returnString += operator(balance, input).toString();
+
+    returnString += operator(balance, input, overdraft).toString();
     return returnString
   }
 
@@ -14,7 +16,12 @@ $(function  () {
       deposit: function (balance, adder) {
         return balance + adder
       },
-      withdraw: function (balance, subtractor) {
+      withdraw: function (balance, subtractor, overdraft) {
+        if (balance - subtractor < 0) {
+          if (!overDraft(subtractor, overdraft)) {
+            return balance
+          }
+        }
         return balance - subtractor
       }
     }
@@ -28,21 +35,48 @@ $(function  () {
     return buttonId.substr(buttonId.indexOf('-') + 1)
   }
 
-  function controller (button) {
-    var account = whichAccout(button.attr('id'))
-    var action  = depositWithdraw(button.attr('id'))
-    var balance = $('#'+ account +'-balance')
-    var input   = $('#'+ account +'-input')
+  function overDraft (subtractor, overdraft) {
+    var overAccount    = $('#'+ overdraft +'-balance')
+    var extraAccount   = overdraft === 'checking' ? 'savings' : 'checking'
+    extraAccount       = $('#'+ extraAccount +'-balance')
 
-    balance = balance.text(handleDollarString(
-      balance.text(),
-      operators(action),
-      input.val()))
+
+    if (Number(overAccount.text().substr(1)) + Number(extraAccount.text().substr(1)) - subtractor >= 0) {
+      var total = handleDollarString(
+        overAccount.text(),
+        operators('deposit'),
+        extraAccount.text().substr(1))
+      var leftOver = handleDollarString(
+        total,
+        operators('withdraw'),
+        subtractor.toString())
+      extraAccount.text(leftOver)
+      if (extraAccount.text() === '$0') {
+        extraAccount.addClass('zero')
+      }
+      return true
+    } else {
+      console.log("too much")
+      return false
+    }
+  }
+
+  function controller (button) {
+    var account       = whichAccout(button.attr('id'))
+    var action        = depositWithdraw(button.attr('id'))
+    var balance       = $('#'+ account +'-balance')
+    var input         = $('#'+ account +'-input')
+    var finalBalance  = handleDollarString(
+                          balance.text(),
+                          operators(action),
+                          input.val(),
+                          account)
+    balance = balance.text(Number(finalBalance.substr(1)) > 0 ? finalBalance : '$0')
+    balance.text() === '$0' ? balance.addClass('zero') : balance.removeClass('zero')
     input.val(null)
   }
 
   $('.button').click(function() {
     controller($(this))
   })
-
 })
